ccc83e72a4f92c39a9a24f823e336cfcd3a5f0b1
Sam Breed <sam@quickleft.com>
2011-07-31 21:11:35 -0600

    refactoring Cakefile into stand alone lib, with some tests

---
+++ bin/tmpl.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var sys = require("sys"),
+    tmpl = require("../lib/index");
+
+var args = process.argv.slice(0);
+// shift off node and script name
+args.shift(); args.shift();
+
+var target_dir = args[0] || process.cwd()
+  , output_dir = args[1] || process.cwd();
+
+tmpl.build( target_dir, function( data ){
+  tmpl.process( data, output_dir );
+});
